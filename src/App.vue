91a9391c31a7b81e4cfc904ad20e7abaf4468b0a
Victor Higaki <higvic@gmail.com>
2019-10-20 16:32:38 -0300

    #3 - Lifecycle Hooks e Computed Properties

--- src/App.vue
+++ src/App.vue
@@ -1,105 +1,114 @@
 <template>
-  <div>
-    <div id="slogan">
-      <div class="text-center">
-        <h1>NameGator</h1>
-        <br />
-        <h6 class="text-secondary">Gerador de nomes utilizando Vue.js, GraphQL e Node</h6>
-      </div>
-    </div>
-    <div id="main">
-      <div class="container">
-        <div class="row">
-          <div class="col-md">
-            <h5>
-              Prefixos
-              <span class="badge badge-info">{{prefixes.length}}</span>
-            </h5>
-            <div class="card">
-              <div class="card-body">
-                <ul class="list-group">
-                  <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
-                    <div class="row">
-                      <div class="col-md">{{ prefix }}</div>
-                      <div class="col-md text-right">
-                        <button class="btn btn-info" v-on:click="deletePrefix(prefix)">
-                          <span class="fa fa-trash"></span>
-                        </button>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-                <br />
-                <div class="input-group">
-                  <input
-                    type="text"
-                    class="form-control"
-                    v-model="prefix"
-                    v-on:keyup.enter="addPrefix(prefix)"
-                    placeholder="Digite o prefixo"
-                  />
-                  <div class="input-group-append">
-                    <button class="btn btn-info" v-on:click="addPrefix(prefix)">
-                      <span class="fa fa-plus"></span>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md">
-            <h5>
-              Sufixos
-              <span class="badge badge-info">{{sufixes.length}}</span>
-            </h5>
-            <div class="card">
-              <div class="card-body">
-                <ul class="list-group">
-                  <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
-                    <div class="row">
-                      <div class="col-md">{{sufix}}</div>
-                      <div class="col-md text-right">
-                        <button class="btn btn-info" v-on:click="deleteSufix(sufix)">
-                          <span class="fa fa-trash"></span>
-                        </button>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-                <br />
-                <div class="input-group">
-                  <input
-                    type="text"
-                    class="form-control"
-                    v-model="sufix"
-                    v-on:keyup.enter="addSufix(sufix)"
-                    placeholder="Digite o sufixo"
-                  />
-                  <div class="input-group-append">
-                    <button class="btn btn-info" v-on:click="addSufix(sufix)">
-                      <span class="fa fa-plus"></span>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <br />
-        <h5>
-          Domains
-          <span class="badge badge-info">{{domains.length}}</span>
-        </h5>
-        <div class="card">
-          <div class="card-body">
-            <ul class="list-group">
-              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">{{domain}}</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div>
+		<div id="slogan">
+			<div class="text-center">
+				<h1>NameGator</h1>
+				<br />
+				<h6 class="text-secondary">Gerador de nomes utilizando Vue.js, GraphQL e Node</h6>
+			</div>
+		</div>
+		<div id="main">
+			<div class="container">
+				<div class="row">
+					<div class="col-md">
+						<h5>
+							Prefixos
+							<span class="badge badge-info">{{prefixes.length}}</span>
+						</h5>
+						<div class="card">
+							<div class="card-body">
+								<ul class="list-group">
+									<li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
+										<div class="row">
+											<div class="col-md">{{ prefix }}</div>
+											<div class="col-md text-right">
+												<button class="btn btn-info" v-on:click="deletePrefix(prefix)">
+													<span class="fa fa-trash"></span>
+												</button>
+											</div>
+										</div>
+									</li>
+								</ul>
+								<br />
+								<div class="input-group">
+									<input
+										type="text"
+										class="form-control"
+										v-model="prefix"
+										v-on:keyup.enter="addPrefix(prefix)"
+										placeholder="Digite o prefixo"
+									/>
+									<div class="input-group-append">
+										<button class="btn btn-info" v-on:click="addPrefix(prefix)">
+											<span class="fa fa-plus"></span>
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md">
+						<h5>
+							Sufixos
+							<span class="badge badge-info">{{sufixes.length}}</span>
+						</h5>
+						<div class="card">
+							<div class="card-body">
+								<ul class="list-group">
+									<li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
+										<div class="row">
+											<div class="col-md">{{sufix}}</div>
+											<div class="col-md text-right">
+												<button class="btn btn-info" v-on:click="deleteSufix(sufix)">
+													<span class="fa fa-trash"></span>
+												</button>
+											</div>
+										</div>
+									</li>
+								</ul>
+								<br />
+								<div class="input-group">
+									<input
+										type="text"
+										class="form-control"
+										v-model="sufix"
+										v-on:keyup.enter="addSufix(sufix)"
+										placeholder="Digite o sufixo"
+									/>
+									<div class="input-group-append">
+										<button class="btn btn-info" v-on:click="addSufix(sufix)">
+											<span class="fa fa-plus"></span>
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<br />
+				<h5>
+					Domains
+					<span class="badge badge-info">{{domains.length}}</span>
+				</h5>
+				<div class="card">
+					<div class="card-body">
+						<ul class="list-group">
+							<li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+								<div class="row">
+									<div class="col-md">{{domain.name}}</div>
+									<div class="col-md text-right">
+										<button class="btn btn-info" v-bind:herf="domain.checkout" target="_blank">
+											<span class="fa fa-shopping-cart"></span>
+										</button>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -113,25 +122,13 @@ export default {
 			prefix: "",
 			sufix: "",
 			prefixes: ["Air", "Jet", "Flight"],
-			sufixes: ["Hub", "Station", "Mart"],
-			domains: [
-				"AirHub",
-				"AirStation",
-				"AirMart",
-				"JetHub",
-				"JetStation",
-				"JetMart",
-				"FlightHub",
-				"FlightStation",
-				"FlightMart"
-			]
+			sufixes: ["Hub", "Station", "Mart"]
 		};
 	},
 	methods: {
 		addPrefix(prefix) {
 			this.prefixes.push(prefix);
 			this.prefix = "";
-			this.generate();
 		},
 		deletePrefix(prefix) {
 			this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
@@ -139,18 +136,27 @@ export default {
 		addSufix(sufix) {
 			this.sufixes.push(sufix);
 			this.sufix = "";
-			this.generate();
 		},
 		deleteSufix(sufix) {
 			this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
-		},
-		generate() {
-			this.domains = [];
+		}
+	},
+	computed: {
+		domains() {
+			// console.log("generating domains...");
+			const domains = [];
 			for (const prefix of this.prefixes) {
 				for (const sufix of this.sufixes) {
-					this.domains.push(prefix + sufix);
+					const name = prefix + sufix;
+					const url = name.toLowerCase();
+					const checkout = `https://checkout.hostgator.com.br/?a=add&sld=${url}&tld=.com.br`;
+					domains.push({
+						name,
+						checkout
+					});
 				}
 			}
+			return domains;
 		}
 	}
 };
@@ -158,12 +164,12 @@ export default {
 
 <style>
 #slogan {
-  margin-top: 30px;
-  margin-bottom: 30px;
+	margin-top: 30px;
+	margin-bottom: 30px;
 }
 #main {
-  background-color: #f1f1f1;
-  padding-top: 30px;
-  padding-bottom: 30px;
+	background-color: #f1f1f1;
+	padding-top: 30px;
+	padding-bottom: 30px;
 }
 </style>
